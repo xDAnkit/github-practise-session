8d40796a2737d6835e42cc7669d10433e69fcafd
xDAnkit <xDAnkitjain@gmail.com>
2022-12-02 21:51:19 +0530

    Rewrite the reverse string logic

--- reverse-string.js
+++ reverse-string.js
@@ -1,6 +1,27 @@
-function reverseString(str) {
-  return str.split().reverse().join("");
-}
+/**
+ * @description Method to reverse a string
+ * @param {String} str
+ * @returns {String}
+ *
+ * Step 1   - Handle edge case or Split the string
+ * Step 2   - Declare a local variable
+ * Step 3   - Iterate a loop to reverse string
+ * Step 4   - Join the character form the last i.e., result = current + result
+ * Step 5   - Return result
+ */
+const reverseString = (str) => {
+  if (!str && typeof str !== "string") {
+    return "Please enter a valid input";
+  }
 
-reverseString("ankit");
-reverseString("tarun");
+  const splittedStr = str.split("");
+  let result = "";
+
+  for (let i = 0; i < splittedStr.length; i++) {
+    result = splittedStr[i] + result;
+  }
+  return result;
+};
+
+console.log(reverseString("ankit"));
+console.log(reverseString("tarun"));
